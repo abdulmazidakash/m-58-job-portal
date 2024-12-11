6e70bc95099515fa3a4113d33124a44df1b96133
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-12-11 23:53:44 +0600

    m-59-3 done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Banner from './Banner';
+import HotJobs from './HotJobs';
 
 const Home = () => {
 	return (
 		<div>
 			<Banner/>
+			<HotJobs></HotJobs>
 		</div>
 	);
 };
