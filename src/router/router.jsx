f613e6f22a3b72f93f8a05f8a3d3beffb59d98ef
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-12-14 17:08:20 +0600

    module complete

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -9,6 +9,7 @@ import JobApply from "../pages/jobApply/JobApply";
 import MyApplications from "../pages/myApplications/MyApplications";
 import AddJob from "../pages/AddJob/AddJob";
 import MyPostedJobs from "../pages/MyPostedJobs/MyPostedJobs";
+import ViewApplications from "../pages/ViewApplications/ViewApplications";
 
 const router = createBrowserRouter([
 	{
@@ -50,6 +51,13 @@ const router = createBrowserRouter([
 				element: <PrivateRoute>
 					<MyPostedJobs></MyPostedJobs>
 					</PrivateRoute>,
+			},
+			{
+				path: '/viewApplications/:job_id',
+				element: <PrivateRoute>
+					<ViewApplications></ViewApplications>
+					</PrivateRoute>,
+				loader: ({params}) => fetch(`http://localhost:5000/job-applications/jobs/${params.job_id}`)
 			},
 			{
 				path: '/register',
